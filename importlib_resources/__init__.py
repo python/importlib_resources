ac47cac90c37e6b9daed525ea5bbae869828eafc
Brett Cannon <brett@python.org>
2017-10-13 15:53:40 -0700

    Missed an f-string

--- importlib_resources/__init__.py
+++ importlib_resources/__init__.py
@@ -8,10 +8,10 @@ from typing.io import BinaryIO
 Path = Union[str, os.PathLike]
 
 
-def _get_package(module_name):
-    module = importlib.import_module(module_name)
+def _get_package(package_name):
+    module = importlib.import_module(package_name)
     if module.__spec__.submodule_search_locations is None:
-        raise TypeError(f"{module_name!r} is not a package")
+        raise TypeError("{!r} is not a package".format(package_name))
     else:
         return module
 
@@ -21,7 +21,7 @@ def _normalize_path(path):
         raise ValueError(f"{path!r} is absolute")
     normalized_path = os.path.normpath(path)
     if normalized_path.startswith(".."):
-        raise ValueError("{path!r} attempts to traverse past package".format(path))
+        raise ValueError("{!r} attempts to traverse past package".format(path))
     else:
         return normalized_path
 
