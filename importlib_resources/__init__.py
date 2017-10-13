403bdc76f4f1ca81b0990df291d899e6ef7c98a6
Brett Cannon <brett@python.org>
2017-10-13 15:42:03 -0700

    Implement open()

---
+++ importlib_resources/__init__.py
@@ -0,0 +1,36 @@
+import importlib
+import io
+import os.path
+from typing import Union
+from typing.io import BinaryIO
+
+
+Path = Union[str, os.PathLike]
+
+
+def _get_package(module_name):
+    module = importlib.import_module(module_name)
+    if module.__spec__.submodule_search_locations is None:
+        raise TypeError(f"{module_name!r} is not a package")
+    else:
+        return module
+
+
+def _normalize_path(path):
+    if os.path.isabs(path):
+        raise ValueError(f"{path!r} is absolute")
+    normalized_path = os.path.normpath(path)
+    if normalized_path.startswith(".."):
+        raise ValueError(f"{path!r} attempts to traverse past package")
+    else:
+        return normalized_path
+
+
+def open(package_name: str, path: Path) -> BinaryIO:
+    """Return a file-like object opened for binary-reading of the resource."""
+    normalized_path = _normalize_path(path)
+    package = _get_package(package_name)
+    package_path = os.path.dirname(os.path.abspath(package.__spec__.origin))
+    full_path = os.path.join(package_path, normalized_path)
+    data = package.__spec__.loader.get_data(full_path)
+    return io.BytesIO(data)
---
+++ importlib_resources/tests/test_open.py
@@ -0,0 +1,59 @@
+import os.path
+import pathlib
+import unittest
+
+import importlib_resources as resources
+
+
+PACKAGE = __spec__.name.rpartition('.')[0]
+
+
+class CommonTests(unittest.TestCase):
+
+    def test_string_path(self):
+        path = 'data/test.file'
+        # Passing in a string for the path should succeed.
+        with resources.open(PACKAGE, path) as file:
+            pass  # No error.
+
+    def test_pathlib_path(self):
+        # Passing in a pathlib.PurePath object for the path should succeed.
+        path = pathlib.PurePath('data')/'test.file'
+        with resources.open(PACKAGE, path) as file:
+            pass  # No error.
+
+    def test_absolute_path(self):
+        # An absolute path is a ValueError.
+        path = pathlib.Path('data/test.file').resolve()
+        with self.assertRaises(ValueError):
+            with resources.open(PACKAGE, path) as file:
+                pass
+
+    def test_traversing_path(self):
+        # A path that traverses -- e.g. has ``..`` -- is a ValueError.
+        path = 'data/../../__init__.py'
+        with self.assertRaises(ValueError):
+            with resources.open(PACKAGE, path) as file:
+                pass
+
+    def test_importing_module_as_side_effect(self):
+        # The anchor package can already be imported.
+        pass
+
+    def test_non_package(self):
+        # The anchor package cannot be a module.
+        with self.assertRaises(TypeError):
+            with resources.open(__spec__.name, 'data/test.file') as file:
+                pass
+
+
+class OpenTests(unittest.TestCase):
+
+    def test_opened_for_reading(self):
+        # The file-like object is ready for reading.
+        with resources.open(PACKAGE, 'data/test.file') as file:
+            self.assertEqual(b"Hello, World!\n", file.read())
+
+
+if __name__ == '__main__':
+    unittest.main()
