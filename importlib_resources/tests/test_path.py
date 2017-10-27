6daf3d416be4a54b8fbb4cbedc833d086b40fe9d
Brett Cannon <brett@python.org>
2017-10-27 12:04:43 -0700

    Test zip data for path()

--- importlib_resources/tests/test_path.py
+++ importlib_resources/tests/test_path.py
@@ -16,14 +16,23 @@ class CommonTests(util.CommonTests, unittest.TestCase):
             pass
 
 
-class PathTests(unittest.TestCase):
+class PathTests:
 
     def test_reading(self):
         # Path should be readable.
         # Test also implicitly verifies the returned object is a pathlib.Path
         # instance.
-        with resources.path(data, 'utf-8.file') as path:
+        with resources.path(self.data, 'utf-8.file') as path:
             # pathlib.Path.read_text() was introduced in Python 3.5.
             with path.open('r', encoding='utf-8') as file:
                 text = file.read()
             self.assertEqual('Hello, UTF-8 world!\n', text)
+
+
+class PathDiskTests(PathTests, unittest.TestCase):
+
+    data = data
+
+
+class PathZipTests(PathTests, util.ZipSetup, unittest.TestCase):
+    pass
