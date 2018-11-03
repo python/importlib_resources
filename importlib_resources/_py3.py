e0cec0c67de0a797ea6b174bf9db44f55ec59341
Jason R. Coombs <jaraco@jaraco.com>
2018-11-03 11:53:19 -0400

    Extract _resolve for resolving a name or package to a module. Reduces complexity and harmonizes py version variance somewhat.

--- importlib_resources/_py3.py
+++ importlib_resources/_py3.py
@@ -22,24 +22,23 @@ else:
     Resource = str                                  # pragma: >=36
 
 
+def _resolve(name) -> ModuleType:
+    """If name is a string, resolve to a module."""
+    if hasattr(name, '__spec__'):
+        return name
+    return import_module(name)
+
+
 def _get_package(package) -> ModuleType:
     """Take a package name or module object and return the module.
 
-    If a name, the module is imported.  If the passed or imported module
+    If a name, the module is imported.  If the resolved module
     object is not a package, raise an exception.
     """
-    if hasattr(package, '__spec__'):
-        if package.__spec__.submodule_search_locations is None:
-            raise TypeError('{!r} is not a package'.format(
-                package.__spec__.name))
-        else:
-            return package
-    else:
-        module = import_module(package)
-        if module.__spec__.submodule_search_locations is None:
-            raise TypeError('{!r} is not a package'.format(package))
-        else:
-            return module
+    module = _resolve(package)
+    if module.__spec__.submodule_search_locations is None:
+        raise TypeError('{!r} is not a package'.format(package))
+    return module
 
 
 def _normalize_path(path) -> str:
