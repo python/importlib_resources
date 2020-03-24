0355a88c68aaf44c855d1e13beea813cb0b2aee7
Jason R. Coombs <jaraco@jaraco.com>
2020-03-24 10:50:42 -0400

    Fix syntax

--- importlib_resources/abc.py
+++ importlib_resources/abc.py
@@ -112,25 +112,25 @@ class Traversable(Protocol):
         """
 
 
-class ResourceReader:
+class SimpleReader:
     # Holds the string name of the virtual Python package this is a resource loader for.
     # For filesystems, this is effectively a reader for a directory at `fullname.replace('.', '/')`
     fullname = None
 
-    def child_readers(self) -> [ResourceReader]
+    def child_readers(self) -> ['SimpleReader']:
         """Obtain an iterable of ResourceReader for available child virtual packages of this one.
 
         On filesystems, this essentially returns instances corresponding to immediate child directories.
         """
 
-    def resources(self) -> [str]
+    def resources(self) -> [str]:
         """Obtain available named resources for this virtual package.
 
         On filesystems, this essentially returns files in the current directory.
         TODO consider returning a special type that exposes an `open()`, etc.
-        """"
+        """
 
-    def open_binary(self, resource) -> File
+    def open_binary(self, resource) -> BinaryIO:
         """Obtain a File-like for a named resource.
 
         On filesystems, this attempts to open os.path.join(self, resource).
