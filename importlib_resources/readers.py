1fe4af88385797fabe05d5b11fe07df7e6cf27ff
Jason R. Coombs <jaraco@jaraco.com>
2020-06-09 21:03:19 -0400

    Ensure native file system paths are returned directly.

--- importlib_resources/readers.py
+++ importlib_resources/readers.py
@@ -7,6 +7,14 @@ class FileReader(abc.TraversableResources):
     def __init__(self, loader):
         self.path = Path(loader.path).parent
 
+    def resource_path(self, resource):
+        """
+        Return the file system path to prevent
+        `resources.path()` from creating a temporary
+        copy.
+        """
+        return str(self.path.joinpath(resource))
+
     def files(self):
         return self.path
 
