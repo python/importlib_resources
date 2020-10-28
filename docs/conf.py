dbe4d9f73cc1f9c6005defeeb829322f967c7053
Jason R. Coombs <jaraco@jaraco.com>
2020-10-28 19:24:34 -0400

    Add hyperlinks to the changelog for issues (including bpo) and PEPs.

--- docs/conf.py
+++ docs/conf.py
@@ -108,13 +108,25 @@ intersphinx_mapping = {
     }
 
 
-# For rst.linker, inject release dates into changelog.rst
 link_files = {
     'changelog.rst': dict(
+        using=dict(GH='https://github.com'),
         replace=[
             dict(
+                pattern=r'(Issue #|\B#)(?P<issue>\d+)',
+                url='{package_url}/issues/{issue}',
+                ),
+            dict(
                 pattern=r'^(?m)((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n',
-                with_scm='{text}\n{rev[timestamp]:%Y-%m-%d}\n\n',
+                with_scm='{text}\n{rev[timestamp]:%d %b %Y}\n',
+                ),
+            dict(
+                pattern=r'PEP[- ](?P<pep_number>\d+)',
+                url='https://www.python.org/dev/peps/pep-{pep_number:0>4}/',
+                ),
+            dict(
+                pattern=r'(Python #|bpo-)(?P<python>\d+)',
+                url='http://bugs.python.org/issue{python}',
                 ),
             ],
         ),
