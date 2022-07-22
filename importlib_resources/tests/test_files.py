94d59b6c5d3ea0290116aae4bcd073e388edc03c
Jason R. Coombs <jaraco@jaraco.com>
2022-07-22 13:38:00 -0400

    Add test capturing current behavior and missed expectation. Ref #203.

--- importlib_resources/tests/test_files.py
+++ importlib_resources/tests/test_files.py
@@ -1,10 +1,13 @@
 import typing
 import unittest
+import contextlib
 
 import importlib_resources as resources
 from importlib_resources.abc import Traversable
 from . import data01
 from . import util
+from . import _path
+from ._compat import os_helper, import_helper
 
 
 class FilesTests:
@@ -40,6 +43,31 @@ class OpenNamespaceTests(FilesTests, unittest.TestCase):
         from . import namespacedata01
 
         self.data = namespacedata01
+
+
+class ModulesFilesTests(unittest.TestCase):
+    def setUp(self):
+        self.fixtures = contextlib.ExitStack()
+        self.addCleanup(self.fixtures.close)
+        self.site_dir = self.fixtures.enter_context(os_helper.temp_dir())
+        self.fixtures.enter_context(import_helper.DirsOnSysPath(self.site_dir))
+        self.fixtures.enter_context(import_helper.CleanImport())
+
+    def test_module_resources(self):
+        """
+        A module can have resources found adjacent to the module.
+        """
+        spec = {
+            'mod.py': '',
+            'res.txt': 'resources are the best',
+        }
+        _path.build(spec, self.site_dir)
+        import mod
+
+        # currently a failure occurs; ref #203
+        with self.assertRaisesRegex(TypeError, '.*mod.* is not a package'):
+            actual = resources.files(mod).joinpath('res.txt').read_text()
+            assert actual == spec['res.txt']
 
 
 if __name__ == '__main__':
