d9e44b65dfb6a51f2ec9ee1e041ff46ca75befef
Jason R. Coombs <jaraco@jaraco.com>
2024-03-16 11:01:33 -0400

    Ensure stdlib readers are excluded on Python 3.10 even when found in importlib.readers. Closes #257.

--- importlib_resources/tests/test_files.py
+++ importlib_resources/tests/test_files.py
@@ -4,8 +4,6 @@ import warnings
 import importlib
 import contextlib
 
-import pytest
-
 import importlib_resources as resources
 from ..abc import Traversable
 from . import data01
@@ -36,7 +34,6 @@ class FilesTests:
     def test_traversable(self):
         assert isinstance(resources.files(self.data), Traversable)
 
-    @pytest.mark.xfail("sys.version_info[:2] == (3, 10)", reason="#257")
     def test_joinpath_with_multiple_args(self):
         files = resources.files(self.data)
         binfile = files.joinpath('subdirectory', 'binary.file')
