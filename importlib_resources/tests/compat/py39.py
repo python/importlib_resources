0db550c38ad9ba4ab4fb6fa8c070c040935e2f33
Jason R. Coombs <jaraco@jaraco.com>
2024-03-20 06:20:54 -0400

    Consolidated test support logic in jaraco.test.cpython.

--- importlib_resources/tests/compat/py39.py
+++ importlib_resources/tests/compat/py39.py
@@ -2,29 +2,9 @@
 Backward-compatability shims to support Python 3.9 and earlier.
 """
 
-import types
+from jaraco.test.cpython import from_test_support, try_import
 
-from jaraco.collections import Projection
-
-
-def from_test_support(*names):
-    """
-    Return a SimpleNamespace of names from test.support.
-    """
-    import test.support
-
-    return types.SimpleNamespace(**Projection(names, vars(test.support)))
-
-
-try:
-    from test.support import import_helper  # type: ignore
-except ImportError:
-    import_helper = from_test_support(
-        'modules_setup', 'modules_cleanup', 'DirsOnSysPath'
-    )
-
-
-try:
-    from test.support import os_helper  # type: ignore
-except ImportError:
-    os_helper = from_test_support('temp_dir')
+import_helper = try_import('import_helper') or from_test_support(
+    'modules_setup', 'modules_cleanup', 'DirsOnSysPath'
+)
+os_helper = try_import('os_helper') or from_test_support('temp_dir')
