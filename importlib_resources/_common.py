a2c72fd65b79f70901be54a526a08ba098ce03a8
Sachaa-Thanasius <111999343+Sachaa-Thanasius@users.noreply.github.com>
2025-04-06 20:03:58 -0400

    Remove lone usage of tempfile at global scope to avoid needing it at import time.

--- importlib_resources/_common.py
+++ importlib_resources/_common.py
@@ -181,23 +181,14 @@ def _(path):
 
 
 @contextlib.contextmanager
-def _temp_path(dir: tempfile.TemporaryDirectory):
-    """
-    Wrap tempfile.TemporaryDirectory to return a pathlib object.
-    """
-    with dir as result:
-        yield pathlib.Path(result)
-
-
-@contextlib.contextmanager
 def _temp_dir(path):
     """
     Given a traversable dir, recursively replicate the whole tree
     to the file system in a context manager.
     """
     assert path.is_dir()
-    with _temp_path(tempfile.TemporaryDirectory()) as temp_dir:
-        yield _write_contents(temp_dir, path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        yield _write_contents(pathlib.Path(temp_dir), path)
 
 
 def _write_contents(target, source):
