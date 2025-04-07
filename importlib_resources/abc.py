edbcd9036e54809889a5d97535341b343ff0f970
Sachaa-Thanasius <111999343+Sachaa-Thanasius@users.noreply.github.com>
2025-04-06 22:22:15 -0400

    Add module-level __getattr__ and TYPE_CHECKING hack to importlib_resources.abc to 1) publicly export Traversable from importlib_resources._traversable and 2) use Traversable in internal annotations, all without importing it eagerly at import time.

--- importlib_resources/abc.py
+++ importlib_resources/abc.py
@@ -1,26 +1,46 @@
 import abc
-import itertools
 import os
-import pathlib
 from typing import (
     Any,
     BinaryIO,
     Iterable,
     Iterator,
     NoReturn,
-    Literal,
-    Optional,
-    Protocol,
     Text,
-    TextIO,
     Union,
-    overload,
-    runtime_checkable,
 )
+
+from ._typing import TYPE_CHECKING
 
 StrPath = Union[str, os.PathLike[str]]
 
 __all__ = ["ResourceReader", "Traversable", "TraversableResources"]
+
+
+# A hack for the following targets:
+# a) Type checkers, so they can understand what __getattr__() exports.
+# b) Internal annotations, so that Traversable can be used in deferred annotations via
+#    _self_mod.Traversable.
+if TYPE_CHECKING:
+    from ._traversable import Traversable
+
+    class _self_mod:
+        from ._traversable import Traversable
+
+else:
+    _self_mod = __import__("sys").modules[__name__]
+
+
+def __getattr__(name: str) -> object:
+    # Defer import to avoid an import dependency on typing, since Traversable subclasses
+    # typing.Protocol.
+    if name == "Traversable":
+        from ._traversable import Traversable
+
+        return Traversable
+
+    msg = f"module {__name__!r} has no attribute {name!r}"
+    raise AttributeError(msg)
 
 
 class ResourceReader(metaclass=abc.ABCMeta):
@@ -69,107 +89,6 @@ class TraversalError(Exception):
     pass
 
 
-@runtime_checkable
-class Traversable(Protocol):
-    """
-    An object with a subset of pathlib.Path methods suitable for
-    traversing directories and opening files.
-
-    Any exceptions that occur when accessing the backing resource
-    may propagate unaltered.
-    """
-
-    @abc.abstractmethod
-    def iterdir(self) -> Iterator["Traversable"]:
-        """
-        Yield Traversable objects in self
-        """
-
-    def read_bytes(self) -> bytes:
-        """
-        Read contents of self as bytes
-        """
-        with self.open('rb') as strm:
-            return strm.read()
-
-    def read_text(
-        self, encoding: Optional[str] = None, errors: Optional[str] = None
-    ) -> str:
-        """
-        Read contents of self as text
-        """
-        with self.open(encoding=encoding, errors=errors) as strm:
-            return strm.read()
-
-    @abc.abstractmethod
-    def is_dir(self) -> bool:
-        """
-        Return True if self is a directory
-        """
-
-    @abc.abstractmethod
-    def is_file(self) -> bool:
-        """
-        Return True if self is a file
-        """
-
-    def joinpath(self, *descendants: StrPath) -> "Traversable":
-        """
-        Return Traversable resolved with any descendants applied.
-
-        Each descendant should be a path segment relative to self
-        and each may contain multiple levels separated by
-        ``posixpath.sep`` (``/``).
-        """
-        if not descendants:
-            return self
-        names = itertools.chain.from_iterable(
-            path.parts for path in map(pathlib.PurePosixPath, descendants)
-        )
-        target = next(names)
-        matches = (
-            traversable for traversable in self.iterdir() if traversable.name == target
-        )
-        try:
-            match = next(matches)
-        except StopIteration:
-            raise TraversalError(
-                "Target not found during traversal.", target, list(names)
-            )
-        return match.joinpath(*names)
-
-    def __truediv__(self, child: StrPath) -> "Traversable":
-        """
-        Return Traversable child in self
-        """
-        return self.joinpath(child)
-
-    @overload
-    def open(self, mode: Literal['r'] = 'r', *args: Any, **kwargs: Any) -> TextIO: ...
-
-    @overload
-    def open(self, mode: Literal['rb'], *args: Any, **kwargs: Any) -> BinaryIO: ...
-
-    @abc.abstractmethod
-    def open(
-        self, mode: str = 'r', *args: Any, **kwargs: Any
-    ) -> Union[TextIO, BinaryIO]:
-        """
-        mode may be 'r' or 'rb' to open as text or binary. Return a handle
-        suitable for reading (same as pathlib.Path.open).
-
-        When opening as text, accepts encoding parameters such as those
-        accepted by io.TextIOWrapper.
-        """
-
-    @property
-    @abc.abstractmethod
-    def name(self) -> str:
-        """
-        The base name of this object without any parent references.
-        """
-
-
 class TraversableResources(ResourceReader):
     """
     The required interface for providing traversable
@@ -177,7 +96,7 @@ class TraversableResources(ResourceReader):
     """
 
     @abc.abstractmethod
-    def files(self) -> "Traversable":
+    def files(self) -> "_self_mod.Traversable":
         """Return a Traversable object for the loaded package."""
 
     def open_resource(self, resource: StrPath) -> BinaryIO:
